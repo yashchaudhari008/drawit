5af61e49733156799fb85db4a1616c8a384accd7
mukulsinghal407 <mukulsinghal407@gmail.com>
2021-10-04 17:02:24 +0530

    Back to Home Alert Message

--- AppData/index.js
+++ AppData/index.js
@@ -67,3 +67,12 @@ function fullScreen() {
     let fs = fullscreen();
     fullscreen(!fs);
 }
+
+function backtohome()
+{
+    if(confirm("Are you sure you want to exit?"))
+    {
+        location.href="../index.html";
+    }
+    else{}
+}
